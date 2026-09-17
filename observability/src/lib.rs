4cce78a0276e4945be448476890d0c31e1032f41
Maksymilian Kowalski <makow.088@gmail.com>
2026-09-18 00:01:53 +0200

    Fix build

--- observability/src/lib.rs
+++ observability/src/lib.rs
@@ -49,7 +49,7 @@ pub fn meter(name: &'static str) -> Meter {
 
 pub fn mixtrics_registry(name: &'static str) -> BoxedRegistry {
     let m = meter(name);
-    let registry = mixtrics::registry::opentelemetry::OpenTelemetryMetricsRegistry::new(m);
+    let registry = mixtrics::registry::opentelemetry_0_31::OpenTelemetryMetricsRegistry::new(m);
     Box::new(registry)
 }
 
